9c57fa0ae07818fb957533ce2d0dc78dc2c65f59
robertotejado <robertotejado@gmail.com>
2014-11-26 11:04:52 +0100

    FastCheck

---
+++ Login/Login/Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Providers.Entities;
+using System.Web.Security;
+
+namespace Login.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User model, string returnUrl)
+        {/*
+            // Lets first check if the Model is valid or not
+            if (ModelState.IsValid)
+            {
+                using (userDbEntities entities = new userDbEntities())
+                {
+                    string username = model.username;
+                    string password = model.password;
+
+                    // Now if our password was enctypted or hashed we would have done the
+                    // same operation on the user entered password here, But for now
+                    // since the password is in plain text lets just authenticate directly
+
+                    bool userValid = entities.Users.Any(user => user.username == username && user.password == password);
+
+                    // User found in the database
+                    if (userValid)
+                    {
+
+                        FormsAuthentication.SetAuthCookie(username, false);
+                        if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                            && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                        {
+                            return Redirect(returnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                    }
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model); */
+            return View();
+        }
+
+        public ActionResult LogOff()
+        {
+           // FormsAuthentication.SignOut();
+
+            return RedirectToAction("Login", "Account");
+        }
+
+
+
+    }
+}
---
+++ Login/Login/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This can be viewed only by authenticated users only";
+            // return RedirectToAction("Login", "Account");
+            return View();
+        }
+
+       /* [Authorize(Roles = "admin")]
+        public ActionResult AdminIndex()
+        {
+            ViewBag.Message = "This can be viewed only by users in Admin role only";
+            return View();
+        }*/
+
+       
+
+    }
+}
